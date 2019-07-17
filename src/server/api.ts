2e00b5a590f4e84ec8bdc5380d6b4350c72a1021
sarahkittyy <sarahohlin1@gmail.com>
2019-07-17 18:36:31 -0400

    added homepage data locked behind token wall

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,13 +1,23 @@
 import express from 'express';
+import authorize from './auth/authorize';
+import login from './auth/login';
 
 // Main api endpoint.
 const api = express.Router();
 api.use(express.json());
+api.use(express.urlencoded({extended: true}));
 
 // Testing
 api.get('/', (req, res) => {
 	res.send('hello there');
 });
+
+api.get('/data', authorize, (req, res) => {
+	res.send('secret homepage data');
+});
+
+// For logging in and generating a token.
+api.post('/login', login);
 
 // Export the api router.
 export default api;
