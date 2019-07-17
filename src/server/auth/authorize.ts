2ce032904a800d4708441f123e6f38cfe96a71fc
sarahkittyy <sarahohlin1@gmail.com>
2019-07-17 18:37:01 -0400

    checks session token

---
+++ src/server/auth/authorize.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import jwt from 'jsonwebtoken';
+
+/**
+ * @brief Authorization middleware that searches for a valid auth token.
+ */
+export default function (req: express.Request, res: express.Response, next: express.NextFunction)
+{
+	// Get the token.
+	let token: string = req.session.token || 'invalid-token';
+	
+	// Validate the token.
+	jwt.verify(token, process.env.SECRET, (err, decoded) => {
+		if(!err)
+		{
+			next();
+		}
+		else
+		{
+			res.status(401).send('Token not valid.');
+		}
+	});
+}
